28b452cc438fba9aeeced5284b74dffb47db8e1b
Aviv Gibli <avivgib1012@gmail.com>
2025-05-15 15:30:16 +0300

    refactor: reorganize logger service and improve logging functionality

--- services/logger.service.js
+++ services/logger.service.js
@@ -1,24 +1,24 @@
 import fs from 'fs'
 
-export const loggerService = {
-    debug(...args) {
-        doLog('DEBUG', ...args)
-    },
-    info(...args) {
-        doLog('INFO', ...args)
-    },
-    warn(...args) {
-        doLog('WARN', ...args)
-    },
-    error(...args) {
-        doLog('ERROR', ...args)
-    }
-}
-
-
 const logsDir = './logs'
 if (!fs.existsSync(logsDir)) {
     fs.mkdirSync(logsDir)
+}
+
+export const logger = {
+    debug(...args) {
+        if (process.env.NODE_NEV === 'production') returns
+        _doLog('DEBUG', ...args)
+    },
+    info(...args) {
+        _doLog('INFO', ...args)
+    },
+    warn(...args) {
+        _doLog('WARN', ...args)
+    },
+    error(...args) {
+        _doLog('ERROR', ...args)
+    }
 }
 
 //define the time format
@@ -27,15 +27,23 @@ function getTime() {
     return now.toLocaleString('he')
 }
 
-function isError(e) {
+function _isError(e) {
     return e && e.stack && e.message
 }
 
-function doLog(level, ...args) {
+function _doLog(level, ...args) {
 
-    const strs = args.map(arg =>
-        (typeof arg === 'string' || isError(arg)) ? arg : JSON.stringify(arg)
-    )
+    const strs = args.map(arg => {
+        if (typeof arg ===  'string') {
+        } else if (_isError(arg)) {
+        } else if (arg instanceof Promise) {
+            arg = 'Promise'
+        } else {
+            arg = JSON.stringify(arg)    
+        }
+        return arg
+    })
+
     var line = strs.join(' | ')
     line = `${getTime()} - ${level} - ${line}\n`
     console.log(line)
