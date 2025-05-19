7294594634d1d56298e28e52836305a467d3d754
Aviv Gibli <avivgib1012@gmail.com>
2025-05-20 01:41:04 +0300

    feat: remove hardcoded database credentials from production configuration

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,3 @@
-// export default {
-//     dbURL: 'mongodb+srv://avivgib1012:m1nSanzA7CU9dZ37@mister-toy-cluster.6xl02cv.mongodb.net/',
-//     dbName: 'toy_db',
-// }
-
 import dotenv from 'dotenv'
 dotenv.config()
 
