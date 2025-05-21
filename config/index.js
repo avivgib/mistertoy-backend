0e4f745c89a05a712ab1d4f834bf3eaa36bf905e
Aviv Gibli <avivgib1012@gmail.com>
2025-05-21 19:37:24 +0300

    feat: remove console logging for environment configuration in index.js

--- config/index.js
+++ config/index.js
@@ -3,13 +3,10 @@ import configDev from './dev.js'
 
 export var config
 
-console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'production') {
     config = configProd
-    console.log('config prod', config)
 } else {
     config = configDev
-    console.log('config dev', config)
 }
 
 // config.isGuestMode = true
